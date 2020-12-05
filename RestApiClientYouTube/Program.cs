4b54584e8aa3736d56452472a60a4f9e9da8fbc2
sangithajk <43965922+sangithajk@users.noreply.github.com>
2020-12-05 22:01:40 +0530

    Add project files.

---
+++ RestApiClientYouTube/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace RestApiClientYouTube
+{
+    class Program
+    {
+        HttpClient client = new HttpClient();
+
+        static  async Task Main(string[] args)
+        {
+            Program obj = new Program();
+            await obj.GetTodoItems();
+        }
+        private async Task GetTodoItems()
+        {
+            string response = await client.GetStringAsync(
+                "https://jsonplaceholder.typicode.com/todos");
+            Console.WriteLine(response);
+        }
+
+    }
+}
